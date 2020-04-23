2434880dfde5d1024b4863cffa117cb217a63874
Geetanjali <59143504+geet3011@users.noreply.github.com>
2020-04-23 15:56:07 +0530

    My Portfolio

---
+++ js/test.js
@@ -0,0 +1,13 @@
+document.write("this is external js");
+document.getElementById('th').innerHTML="This is a para";
+var a=4;
+var b=5;
+var a1="this is string";
+document.write(a+a1);
+document.write(a+b);
+console.log("this is console");
+
+var age = alert("tell me your age");
+var id = prompt("tell me your id");
+/*document.write("your id is ",id);*/
+alert("your id is ",id);
